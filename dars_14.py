9a217229e714fb8f6614ed6de3387825898873a6
Abdulmutallibjon <smurfalinea@gmail.com>
2022-08-21 16:57:41 +0300

    yangilandi

--- dars_14.py
+++ dars_14.py
@@ -1,10 +1,40 @@
-# 18-dars: WHILE VA RO'YXATLAR
-
-savat = []
-while True:
-    mahsulot = input("Savatga mahsulot qo'shing:")
-    savat.append(mahsulot)
-    javob = input("Yana mahsulot qo'shasizmi?(ha/yo'q)")
-    if javob != "ha":
-        break
-print("Dastur tugadi!")
+# 18 - dars: WHILE VA RO'YXATLAR
+#
+# savat = []
+# while True:
+#     mahsulot = input("Savatga mahsulot qo'shing:")
+#     savat.append(mahsulot)
+#     javob = input("Yana mahsulot qo'shasizmi?(ha/yo'q)")
+#     if javob != "ha":
+#         break
+# print("Dastur tugadi!")
+#
+# print("Do'stlaringiz yoshini saqlaymiz.")
+# dostlar = {}
+# ishora = True
+# while ishora:
+#     ism = input("Do'stingiz ismini kiriting: ")
+#     yosh = input(f"{ism.title()}ning yoshini kiriting: ")
+#     dostlar[ism] = int(yosh)
+#
+#     javob = input("Yana ma'lumot qo'shasizmi? (ha/yo'q)")
+#     if javob == "yo'q":
+#         ishora = False
+#
+# for ism, yosh in dostlar.items():
+#     print(f"{ism.title()} {yosh} yoshda")
+#
+# cars = ["lacetti", "nexia", "toyota", "nexia", "audi", "malibu", "nexia", "lacetti"]
+# car = "lacetti"
+# while car in cars:
+#     cars.remove(car)
+#
+# print(cars)
+#
+# talabalar = ["hasan", "husan", "olim", "botir"]
+# baholangan_talabalar = {}
+# while talabalar:
+#     talaba = talabalar.pop()
+#     baho = input(f"{talaba.title()}ning bahosini kiriting: ")
+#     print(f"{talaba.title()} baholandi")
+#     baholangan_talabalar[talaba] = int(baho)
